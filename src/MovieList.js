b8b762d8eb0b8dca58ccb0c10b7f20019b79f4dd
Kelly Kaufman <kellykaufman12@gmail.com>
2022-06-15 13:01:44 -0700

    Adds a movie component

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Movie from "./Movie";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([
@@ -9,7 +10,7 @@ const MovieList = () => {
   return (
     <div>
       {movies.map((movie) => (
-        <li>{movie.name}</li>
+        <Movie />
       ))}
     </div>
   );
